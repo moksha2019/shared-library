6bc7ec9884c15816d5291863d34e56785362599a
moksha2019 <81618348+moksha2019@users.noreply.github.com>
2025-06-09 07:28:14 +0530

    added  code to  to calculator file

--- src/com/prasadacademy/builds/calculator.groovy
+++ src/com/prasadacademy/builds/calculator.groovy
@@ -1,1 +1,21 @@
-package com.
+package com.prasadacademy.builds;
+
+// all the methods will be written here
+class  Calculator() {
+    def jenkins
+    Calculator(jenkins){
+        this.jenkins= jenkins
+    }
+
+// Addition method
+def  add(Firstnumber, Secondnumber){
+    // logical code
+    return Firstnumber+Secondnumber
+}
+
+// Substraction method
+def sub(Firstnumber, Secondnumber){
+    // logical code
+    return Firstnumber-Secondnumber
+}
+}
